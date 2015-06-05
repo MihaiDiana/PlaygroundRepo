0a8b0b9d748592d5e8c47e70f6d7bf14ac862981
MihaiDiana <diana_lore07@yahoo.com>
2015-06-05 23:03:14 +0300

    second commit splitted

---
+++ Playground/PlaygroundViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlaygroundViewController.m
+//  Playground
+//
+//  Created by Diana Mihai on 05/06/15.
+//  Copyright (c) 2015 Diana. All rights reserved.
+//
+
+#import "PlaygroundViewController.h"
+
+@interface PlaygroundViewController ()
+
+@end
+
+@implementation PlaygroundViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
